30c299e040bf6fb1def2d099e1e1d35eea125155
logmar98 <marwanarbib15@gmail.com>
2024-02-17 12:50:46 +0100

    update task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """display C"""
-    return "C {}".format(escape(text))
+    return "C {}".format(escape(text.replace("_", " ")))
 
 
 if __name__ == '__main__':
