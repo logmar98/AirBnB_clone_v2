0569c4ed26a0d905f7b098156305195a49899c5c
logmar98 <marwanarbib15@gmail.com>
2024-02-17 13:36:54 +0100

    update task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""start flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """display C"""
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """display python"""
+    return "Python {}".format(escape(text.replace("_", " ")))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def python(n):
+    """display python"""
+    if (isinstance(n, int)):
+        return "{} is a number".format(escape(n.replace("_", " ")))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
