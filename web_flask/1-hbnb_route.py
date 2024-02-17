8e275d2ad3c2f3b73d68e95f1e2ff5501ed6b64a
logmar98 <marwanarbib15@gmail.com>
2024-02-17 12:31:02 +0100

    pycodestyle task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """display “HBNB”'"""
+    """display HBNB"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
