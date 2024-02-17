2c66a694382aa3e0239f08a1b677cbba1ac141b3
logmar98 <marwanarbib15@gmail.com>
2024-02-17 12:40:58 +0100

    update task2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""start flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb(text):
+    """display C"""
+    return f'C {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
